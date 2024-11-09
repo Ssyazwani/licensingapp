e53a9f69191a0e2bba1ee58637f32ae0ed7ad21c
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2024-11-09 21:14:12 +0800

    added types

--- mastertable.cpp
+++ mastertable.cpp
@@ -6,5 +6,12 @@ int main() {
   cout << "For current appliants, press 1 \n";
   cout << "For existing licensees, press 2 \n";
   cout << "For ceased/withdrawn applications, press 3\n";
+
+
+   cout << "MPI applications\n";
+   cout << "SPI applications\n";
+   cout << "MC applications\n";
   return 0;
 }
+
+
