acd3b1613c14436ff210ff4388b9b24eb56af73f
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2024-11-09 21:12:39 +0800

    intial commit

---
+++ mastertable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Welcome to the Licensing Applications\n";
+  cout << "For current appliants, press 1 \n";
+  cout << "For existing licensees, press 2 \n";
+  cout << "For ceased/withdrawn applications, press 3\n";
+  return 0;
+}
